b4e94da0b23cc150e6a884ad99a7e0b675be9e3d
= <=>
2023-06-30 02:20:23 +0800

    3

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -76,5 +76,5 @@ def main():
     model = NeuralNetwork('VGG16')  # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
-    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
+    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth', map_location='cpu'))
     return model
